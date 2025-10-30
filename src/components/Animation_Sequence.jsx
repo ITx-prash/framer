ae44d17d003f774631b195d835bdeb1cfbcfd7b7
Prashant Adhikari <prashantadhikari440@gmail.com>
2025-10-31 00:52:56 +0545

    fun: play with animation sequence and it's smh

--- src/components/Animation_Sequence.jsx
+++ src/components/Animation_Sequence.jsx
@@ -1,88 +1,128 @@
 import { motion, useAnimate } from "motion/react";
+import { button } from "motion/react-client";
 
 const Animation_Sequence = () => {
   const [scope, animate] = useAnimate();
-  const startAnimating = async () => {
-    // initial loader
-    await animate(
-      ".loader",
-      {
-        width: "2rem",
-      },
-      {
-        duration: 0,
-      },
-    );
-    // rotate loader
-    await animate(
-      ".loader",
-      {
-        rotate: 360 * 3,
-      },
-      {
-        duration: 2.5,
-      },
-    );
 
-    // hide loader
-    animate(
-      ".loader",
-      {
-        width: 0,
-      },
-      {
-        duration: 0.1,
-      },
-    );
+  //   ===========Preferred Approach===========
+  //   we can define an array of array which will call each animation in sequence, if confused check the commented code below
 
-    animate(
-      ".text",
-      {
-        display: "none",
-      },
-      { duration: 0.02 },
-    );
+  //   f*ck this animation sequence took me so long even not working properly with as like the alternate approach, itz fkng smh but i won't gu, will come back even stronger on this..peace out
 
-    await animate(
+  // FIXME:
+  /*
+  const animationSequence = [
+    [".loader", { opacity: 1, width: "2rem" }, { duration: 0 }],
+    [".loader", { rotate: 360 * 3 }, { duration: 2.5 }],
+
+    [".loader", { opacity: 0, width: 0 }, { duration: 0.01 }],
+    [".text", { display: "none" }, { duration: 0.3 }, { at: "-1" }],
+    ["button", { width: "5rem" }, { duration: 0.4 }, { at: "-0.9" }],
+    ["button", { borderRadius: "100px" }, { duration: 2 }, {at: "-5"}],
+    [
       "button",
-      { width: "5rem", borderRadius: "1000px" },
-      {
-        duration: 0.3,
-      },
-    );
-
-    // since we are inside the async function, we can await the previous animation to finish and can also chain animations
-    await animate(
-      "button",
-
       {
         opacity: 1,
         scale: [1, 1.2, 0.8, 1],
         backgroundImage: "linear-gradient(to right, #11998e, #38ef7d)",
       },
-      {
-        duration: 0.8,
-      },
-    );
-    animate(
-      ".check-icon",
-      {
-        opacity: 1,
-      },
-      { duration: 0.1 },
-    );
-
-    animate(
+      { duration: 0.8 },
+    ],
+    [".check-icon", { opacity: 1 }, { duration: 0.1 }],
+    [
       ".check-icon path",
-      {
-        pathLength: 1,
-      },
-      {
-        duration: 0.5,
-        ease: "easeInOut",
-      },
-    );
+      { pathLength: 1 },
+      { duration: 0.5, ease: "easeInOut" },
+    ],
+  ];
+
+  const startAnimating = () => {
+    animate(animationSequence);
   };
+*/
+  //   ===========Alternate Approach===========
+
+    const startAnimating = async () => {
+      // initial loader
+      await animate(
+        ".loader",
+        {
+          width: "2rem",
+        },
+        {
+          duration: 0,
+        },
+      );
+      // rotate loader
+      await animate(
+        ".loader",
+        {
+          rotate: 360 * 3,
+        },
+        {
+          duration: 2.5,
+        },
+      );
+
+      // hide loader
+      animate(
+        ".loader",
+        {
+          width: 0,
+        },
+        {
+          duration: 0.1,
+        },
+      );
+
+      animate(
+        ".text",
+        {
+          display: "none",
+        },
+        { duration: 0.02 },
+      );
+
+      await animate(
+        "button",
+        { width: "5rem", borderRadius: "1000px" },
+        {
+          duration: 0.3,
+        },
+      );
+
+      // since we are inside the async function, we can await the previous animation to finish and can also chain animations
+      await animate(
+        "button",
+
+        {
+          opacity: 1,
+          scale: [1, 1.2, 0.8, 1],
+          backgroundImage: "linear-gradient(to right, #11998e, #38ef7d)",
+        },
+        {
+          duration: 0.8,
+        },
+      );
+      animate(
+        ".check-icon",
+        {
+          opacity: 1,
+        },
+        { duration: 0.1 },
+      );
+
+      animate(
+        ".check-icon path",
+        {
+          pathLength: 1,
+        },
+        {
+          duration: 0.5,
+          ease: "easeInOut",
+        },
+      );
+    };
   return (
     <div
       ref={scope}
